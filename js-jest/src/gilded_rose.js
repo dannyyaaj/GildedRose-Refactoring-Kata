3480d4898aa1debd723ebb287f294823336ad89d
Danny Yang <dannyyaaj@gmail.com>
2024-09-12 21:37:54 -0500

    refactor code

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -6,6 +6,48 @@ class Item {
   }
 }
 
+const ItemHandler = {
+  'Aged Brie': (item) => {
+    item.sellIn -= 1
+
+    if (item.quality < 50) {
+      item.quality += 1
+    }
+  },
+  'Sulfuras, Hand of Ragnaros': (_item) => { },
+  'Backstage passes to a TAFKAL80ETC concert': (item) => {
+    item.sellIn -= 1
+
+    if (item.sellIn >= 11) {
+      item.quality += 1
+    } else if (item.sellIn >= 6) {
+      item.quality += 2
+    } else if (item.sellIn > 0) {
+      item.quality += 3
+    } else {
+      item.quality = 0
+    }
+
+    if (item.quality > 50) {
+      item.quality = 50
+    }
+  },
+  'default': (item) => {
+    item.sellIn -= 1
+
+    if (item.sellIn > 0) {
+      item.quality -= 1
+    } else {
+      item.quality -= 2
+    }
+
+    if (item.quality < 0) {
+      item.quality = 0
+    }
+  }
+
+}
+
 class Shop {
   constructor(items = []) {
     this.items = items
@@ -13,48 +55,8 @@ class Shop {
   updateQuality() {
 
     this.items.forEach(item => {
-      switch (item.name) {
-        case 'Sulfuras, Hand of Ragnaros':
-          break
-        case 'Aged Brie':
-          item.sellIn -= 1
-
-          if (item.quality < 50) {
-            item.quality += 1
-          }
-          break
-        case 'Backstage passes to a TAFKAL80ETC concert':
-          item.sellIn -= 1
-
-          if (item.sellIn >= 11) {
-            item.quality += 1
-          } else if (item.sellIn >= 6) {
-            item.quality += 2
-          } else if (item.sellIn > 0) {
-            item.quality += 3
-          } else {
-            item.quality = 0
-          }
-
-          if (item.quality > 50) {
-            item.quality = 50
-          }
-
-          break
-        default:
-          item.sellIn -= 1
-
-          if (item.sellIn > 0) {
-            item.quality -= 1
-          } else {
-            item.quality -= 2
-          }
-
-          if (item.quality < 0) {
-            item.quality = 0
-          }
-          break
-      }
+      const handleItemName = Object.hasOwn(ItemHandler, item.name) ? item.name : 'default'
+      ItemHandler[handleItemName](item)
     })
     return this.items
   }
